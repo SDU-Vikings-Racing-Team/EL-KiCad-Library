7974a8cb19f6f661e81caa6a99ffab4b33bd6419
MV <mving23@student.sdu.dk>
2025-09-19 09:18:20 +0200

    Adding initial functions needed for library submodule structure

---
+++ scripts/setup_project.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from pathlib import Path
+from datetime import datetime
+
+def backup()
+    # ToDo
+    return
+
+def find_lib_root()
+    # ToDo
+    return
+
+def scan_symbols()
+    # ToDo
+    return
+
+def scan_footprints()
+    # ToDo
+    return
+
+def build_sym_tables()
+    # ToDo
+    return
+
+def build_fp_tables()
+    # ToDo
+    return
+
+def main()
+    # ToDo
+    return
+
+if __main__ == "__main__":
+    main()
